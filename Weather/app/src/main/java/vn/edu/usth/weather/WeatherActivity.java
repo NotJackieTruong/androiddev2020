c6ba0e6c5676ca29aaa3bd09b1b12369da0b3c52
khanh <khanhtq.b8084@st.usth.edu.vn>
2019-11-09 09:09:56 +0700

    Practical work 1

---
+++ Weather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -0,0 +1,22 @@
+package vn.edu.usth.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("My_Tag", "")
+    }
+}
