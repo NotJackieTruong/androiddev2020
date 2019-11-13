c1d4ca09f700a1103b274e2e3a71fcc87b866284
khanh <khanhtq.b8084@st.usth.edu.vn>
2019-11-13 15:08:23 +0700

    fixing labwork 4

--- Weather/app/src/main/java/vn/edu/usth/weather/forecast_fragment.java
+++ Weather/app/src/main/java/vn/edu/usth/weather/forecast_fragment.java
@@ -8,41 +8,48 @@ import androidx.fragment.app.Fragment;
 import android.content.res.Resources;
 import android.media.Image;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class forecast_fragment extends Fragment {
     public forecast_fragment(){};
-//
-//    @Nullable
-//    @Override
-//    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-//        //text view
-//        TextView dayText = new TextView((getActivity()));
-//        dayText.setText("Thursday");
-//
-//        //image view
-//        ImageView icon = new ImageView(getActivity());
-//        icon.setImageResource(R.drawable.weather_icon_png_4);
-//
-//        View view  = new View(getContext());
-//        view.setBackgroundColor(0x2000FF00);
-//
-//        LinearLayout linear_layout1 = view.findViewById(R.id.linearlayout1);
-//        linear_layout1.setOrientation(LinearLayout.VERTICAL);
-//        linear_layout1.addView(dayText);
-//        linear_layout1.addView(icon);
-//        return view;
-//    }
-//
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.forecast_fragment, container, false);
+        //text view
+        TextView dayText = new TextView((getActivity()));
+        dayText.setText("Thursday");
+        dayText.setBackgroundColor(0xd85de8);
+        dayText.setPadding(0, 15, 0, 15);
+
+        //image view
+        ImageView icon = new ImageView(getActivity());
+        icon.setImageResource(R.drawable.weather_icon_png_4);
+        icon.setScaleType(ImageView.ScaleType.FIT_CENTER);
+
+//        View view  = new View(getContext());
+//        view.setBackgroundColor(0x2000FF00);
+
+        RelativeLayout.LayoutParams text_view_param = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        text_view_param.addRule(RelativeLayout.CENTER_IN_PARENT);
+        RelativeLayout.LayoutParams image_view_param = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+        View view = inflater.inflate(R.layout.forecast_fragment, container, false);
+        view.setBackgroundColor(0x2000FF00);
+
+        LinearLayout linear_layout1 = view.findViewById(R.id.linearlayout1);
+        linear_layout1.setOrientation(LinearLayout.VERTICAL);
+        linear_layout1.setGravity(Gravity.CENTER);
+        linear_layout1.addView(dayText,text_view_param);
+        linear_layout1.addView(icon, image_view_param);
+        return view;
     }
+
 }
