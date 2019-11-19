65dab1efdf29bbaa628aa49f9dba8790d151afe1
khanh <khanhtq.b8084@st.usth.edu.vn>
2019-11-19 09:44:57 +0700

    practical work 7

--- Weather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ Weather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,24 +1,32 @@
 package vn.edu.usth.weather;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.util.Log;
 
 
 public class WeatherActivity extends AppCompatActivity {
+    private ViewPager viewPager;
+    private Adapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
         Log.i("My_Tag", "This is on start");
+        viewPager = findViewById(R.id.weatherForecastViewPager);
+        adapter = new Adapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
 
-        forecast_fragment forecastFragment = new forecast_fragment();
-        getSupportFragmentManager().beginTransaction().add(R.id.fragment2, forecastFragment).commit();
-
-        WeatherFragment weatherFragment = new WeatherFragment();
-        getSupportFragmentManager().beginTransaction().add(R.id.weatherFragment, weatherFragment).commit();
+//        forecast_fragment forecastFragment = new forecast_fragment();
+//        getSupportFragmentManager().beginTransaction().add(R.id.fragment2, forecastFragment).commit();
+//
+//        WeatherFragment weatherFragment = new WeatherFragment();
+//        getSupportFragmentManager().beginTransaction().add(R.id.weatherFragment, weatherFragment).commit();
     }
 
     @Override
